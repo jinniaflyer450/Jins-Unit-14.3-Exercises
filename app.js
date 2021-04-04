bb91e08d14287aabb4dac87dc9d65c9e48561850
Elizabeth Watson <elizabethwats0n450@gmail.com>
2021-04-04 09:02:03 -0500

    test

--- app.js
+++ app.js
@@ -1,5 +1,12 @@
 async function searchGifs(query){
     const queryInfo = {'params': {'api_key': 'o8xV4J93zC4iYEJnSUdj0fEfWYYgBd5f', 'q': query}};
-    let gif = await axios.get('https://api.giphy.com/v1/gifs/search', queryInfo);
-    console.log(gif);
+    let gif = await axios.get('http://api.giphapi.giphy.com/v1/gifs/random', queryInfo);
+    return gif;
 }
+
+const form = document.querySelector('#gif-search-form');
+form.addEventListener('submit', function(e){
+    e.preventDefault();
+    const query = document.querySelector('#search').value;
+    console.log(searchGifs(query));
+})
