bc3f50aabba3eb956b861af0db373afec6628c90
Elizabeth Watson <elizabethwats0n450@gmail.com>
2021-04-04 09:40:06 -0500

    figured out appending images

--- app.js
+++ app.js
@@ -1,12 +1,18 @@
 async function searchGifs(query){
-    const queryInfo = {'params': {'api_key': 'o8xV4J93zC4iYEJnSUdj0fEfWYYgBd5f', 'q': query}};
-    let gif = await axios.get('http://api.giphapi.giphy.com/v1/gifs/random', queryInfo);
+    const queryInfo = {'params': {'api_key': 'o8xV4J93zC4iYEJnSUdj0fEfWYYgBd5f', 'tag': query}};
+    let gif = await axios.get('http://api.giphy.com/v1/gifs/random', queryInfo);
     return gif;
 }
 
 const form = document.querySelector('#gif-search-form');
-form.addEventListener('submit', function(e){
+form.addEventListener('submit', async function(e){
     e.preventDefault();
     const query = document.querySelector('#search').value;
-    console.log(searchGifs(query));
+    let gif = await searchGifs(query);
+    let gifUrl = gif.data.data.image_url;
+    console.log(gif);
+    let newGif = document.createElement('img');
+    newGif.src = gifUrl;
+    let gifs = document.querySelector('#gifs');
+    gifs.append(newGif);
 })
