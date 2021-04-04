2f8eed21a3add45fadeace9aa18562db9734d10c
Elizabeth Watson <elizabethwats0n450@gmail.com>
2021-04-04 10:03:01 -0500

    Bootstrap added; final touches

--- app.js
+++ app.js
@@ -4,15 +4,25 @@ async function searchGifs(query){
     return gif;
 }
 
+//https://stackoverflow.com/questions/62491649/google-chrome-corb-blocking-giphy-api-images Figured out why my images weren't working here.
+
 const form = document.querySelector('#gif-search-form');
 form.addEventListener('submit', async function(e){
     e.preventDefault();
     const query = document.querySelector('#search').value;
     let gif = await searchGifs(query);
     let gifUrl = gif.data.data.image_url;
-    console.log(gif);
     let newGif = document.createElement('img');
     newGif.src = gifUrl;
+    newGif.classList.add('col-4')
+    newGif.classList.add('m-0')
     let gifs = document.querySelector('#gifs');
     gifs.append(newGif);
+})
+
+const gifsButton = document.querySelector('#remove-all');
+const gifs = document.querySelector('#gifs');
+gifsButton.addEventListener('click', function(e){
+    e.preventDefault();
+    gifs.innerHTML = '';
 })
